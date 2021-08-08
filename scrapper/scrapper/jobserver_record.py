ffe0d8e37c2e39caca443ea67668b8efa9befd4e
Diego <dpalma@evernote.com>
2021-08-08 16:23:57 -0400

    Create job to import data to DB (#14)

---
+++ scrapper/scrapper/jobserver_record.py
@@ -0,0 +1,25 @@
+"""Schema definitions for Jobserver."""
+from mongoengine import DateTimeField
+from mongoengine import Document
+from mongoengine import EmbeddedDocument
+from mongoengine import EmbeddedDocumentField
+from mongoengine import IntField
+from mongoengine import ListField
+from mongoengine import StringField
+
+
+class CountryJobCount(EmbeddedDocument):
+    """Represent a country in a job distribution by country."""
+
+    name = StringField()
+    jobs = IntField()
+
+
+class JobserverRecord(Document):
+    """Represent a record based on schema."""
+
+    name = StringField()
+    date = DateTimeField()
+    jobs_total = IntField()
+    countries = ListField(EmbeddedDocumentField(CountryJobCount))
+    meta = {"indexes": [("name", "date")]}
