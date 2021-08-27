db095fe574f65a3342975c95070561171476f5ee
Diego <dpalma@evernote.com>
2021-08-27 15:37:24 -0400

    Add rate limiter to server (#39)

--- jobservatory_express/app.js
+++ jobservatory_express/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const rateLimit = require('express-rate-limit');
 const morgan = require("morgan");
 const cors = require("cors");
 const technologies = require("./routes/api/technologies");
@@ -8,6 +9,13 @@ function createApp() {
   const app = express();
   app.use(express.json());
   app.use(cors());
+
+  // Limit to 15 requests per minute
+  const apiRequestLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 15
+  });
+  app.use(apiRequestLimiter);
 
   if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
