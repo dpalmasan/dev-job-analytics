f986b02d70a04eac0a1ee73c97128662369e7f7f
djaume <djaume@evernote.com>
2021-08-29 15:35:00 -0400

    fix(): Clean

--- jobservatory_express/controllers/technologies-controller.js
+++ jobservatory_express/controllers/technologies-controller.js
@@ -101,26 +101,3 @@ exports.getTechnologiesByCountry = async (req, res, next) => {
       .json({ success: false, error: `Server error ${error}` });
   }
 };
-
-//@route GET /api/v1/technologies/countries
-exports.getTechnologiesByCountry = async (req, res, next) => {
-  try {
-    var startdate = moment();
-    startdate = startdate.subtract(2, "days");
-    startdate = startdate.format();
-    const technologies = await Technology.find({
-      date: {
-        $gte: startdate,
-      },
-    });
-    return res.status(200).json({
-      success: true,
-      count: technologies.length,
-      data: technologies,
-    });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ success: false, error: `Server error ${error}` });
-  }
-};
