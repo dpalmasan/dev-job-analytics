b6228e3eaa61214ac0e3eaeb635decfa1120a232
djaume <djaume@evernote.com>
2021-09-01 10:57:14 -0400

    fix(): Uncomment SearchBar component

--- jobservatory_react/src/components/search-bar/SearchBar.tsx
+++ jobservatory_react/src/components/search-bar/SearchBar.tsx
@@ -6,35 +6,34 @@ interface SearchBarProps {
   fetchTechByName: (searchValue: string) => Promise<void>;
 }
 export const SearchBar: FC<SearchBarProps> = ({ fetchTechByName }) => {
-  // const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState('');
 
-  // const handleKeyDown = (event: any) => {
-  //   if (event.key === "Enter") {
-  //     fetchTechByName(searchValue);
-  //   }
-  // };
+  const handleKeyDown = (event: any) => {
+    if (event.key === 'Enter') {
+      fetchTechByName(searchValue);
+    }
+  };
 
-  // return (
-  //   <InputGroup
-  //     size="lg"
-  //     borderColor="grey"
-  //     color={theme.colors.teal}
-  //     style={{ paddingRight: 10 }}
-  //     onKeyPress={(e) => handleKeyDown(e)}
-  //   >
-  //     <InputLeftElement
-  //       color={theme.colors.teal}
-  //       pointerEvents="none"
-  //       fontSize="1.2em"
-  //       children={<SearchIcon color="green.500" />}
-  //     />
-  //     <Input
-  //       color={theme.colors.teal}
-  //       value={searchValue}
-  //       onChange={(e) => setSearchValue(e.target.value)}
-  //       placeholder="Add technologies: React, Ruby ..."
-  //     />
-  //   </InputGroup>
-  // );
-  return null;
+  return (
+    <InputGroup
+      size='lg'
+      borderColor='grey'
+      color={theme.colors.teal}
+      style={{ paddingRight: 10 }}
+      onKeyPress={(e) => handleKeyDown(e)}
+    >
+      <InputLeftElement
+        color={theme.colors.teal}
+        pointerEvents='none'
+        fontSize='1.2em'
+        children={<SearchIcon color='green.500' />}
+      />
+      <Input
+        color={theme.colors.teal}
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        placeholder='Add technologies: React, Ruby ...'
+      />
+    </InputGroup>
+  );
 };
